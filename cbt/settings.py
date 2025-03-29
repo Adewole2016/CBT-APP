13000dde554ebc474a961d8f665c8c4b11395bca
Adewole2016 <kunleadewole2008@gmail.com>
2025-03-29 23:51:00 +0100

    Update settings.py

--- cbt/settings.py
+++ cbt/settings.py
@@ -39,7 +39,8 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = env("ALLOWED_HOSTS")
+ALLOWED_HOSTS = ["cbt-app-aix6.onrender.com", "localhost", "127.0.0.1"]
+
 
 #DEBUG = False
 
